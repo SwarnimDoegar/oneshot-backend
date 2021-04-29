e17193a7ea88635ab360d36f073ac92c853c4a9a
Swarnim Doegar <swarnimdoegar@gmail.com>
2021-04-30 02:17:04 +0530

    Added cors to express

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("../db/operations");
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
